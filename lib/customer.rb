560ddc75924a8aabb62cea1b145f576f403e6a61
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-11 19:33:45 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -18,6 +18,16 @@ def new_meal(waiter, total , tip = 0)
   Meal.new(waiter, self, total, tip)
 end 
 
+def meals
+  Meal.all.select do |meal|
+    meal.customer == self 
+  end 
+end 
 
+def waiters
+  meals.map do |meal|
+    meal.waiters
+  end 
+end 
   
 end
