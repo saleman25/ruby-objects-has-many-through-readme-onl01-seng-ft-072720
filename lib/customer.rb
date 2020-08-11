fefe00fee5cf1152ba8df204e9c91e1767bf52a4
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-11 19:37:50 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -29,5 +29,22 @@ def waiters
     meal.waiters
   end 
 end 
+
+def new_meal_20_percent(waiter, total)
+  tip = total * 0.2 
+  Meal.new(waiter, self, total, tip)
+end 
+
+def self.oldest_customer 
+  oldest_age = 0 
+  oldest_customer = nil 
+  self.all.each do |customer| 
+    if customer.age > oldest_age
+      oldest_age = customer.age 
+      oldest_customer = customer 
+    end 
+  end
+  oldest_customer 
+end 
   
 end
